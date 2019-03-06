4d347d491f5873494430b66bb46d9617441ac595
Stefanos Kostaras <stefanoskostaras@hotmail.com>
2019-03-06 23:39:13 +0200

    finished demo project

--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -15,20 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
     
     @IBOutlet weak var animalSoundLabel: UILabel!
     
     @IBAction func catButtonTapped(_ sender: Any) {
         animalSoundLabel.text = "Meow"
+        meowSound.play()
     }
     
     @IBAction func dogButtonTapped(_ sender: Any) {
-        animalSoundLabel.text = "Waf!"
+        animalSoundLabel.text = "Woof!"
+        woofSound.play()
     }
     
     @IBAction func cowButtonTapped(_ sender: Any) {
         animalSoundLabel.text = "Moo!"
+        mooSound.play()
     }
+    
+//    let sound = SimpleSound(named: "Moo!")
+ 
+//    sound.
     
 }
 
